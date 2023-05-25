2e3713470c5c7a829e44180f99f2487512a2598f
Lee-Backend-Developer <eotmd1016@gmail.com>
2023-05-25 11:26:47 +0900

    반응형 기초 (Reactivity) 수강 완료

---
+++ src/lectures/7/App_reactive.vue
@@ -0,0 +1,31 @@
+<template>
+	<div>
+		<button v-on:click="incremeent">Click {{ state.count }}</button>
+		<button v-on:click="incremeent">Deep Click {{ state.deep.count }}</button>
+	</div>
+</template>
+
+<script>
+import { reactive } from 'vue';
+
+export default {
+	setup() {
+		const state = reactive({
+			count: 0,
+			deep: {
+				count: 0,
+			},
+		});
+		const incremeent = () => {
+			state.count++;
+			state.deep.count++;
+		};
+		return {
+			state,
+			incremeent,
+		};
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>

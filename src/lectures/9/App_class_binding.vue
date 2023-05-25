cf09ec1fac5c87a45d5e696ff10ab80e258119ba
Lee-Backend-Developer <eotmd1016@gmail.com>
2023-05-25 16:54:48 +0900

    Class와 Style 바인딩

---
+++ src/lectures/9/App_class_binding.vue
@@ -0,0 +1,55 @@
+<template>
+	<div>
+		<div class="text" :class="classObject">텍스트 입니다.</div>
+		<button v-on:click="toggle">toggle</button>
+		<button v-on:click="hasError = !hasError">toggleError</button>
+	</div>
+</template>
+
+<script>
+import { computed } from '@vue/reactivity';
+import { reactive, ref } from 'vue';
+
+export default {
+	setup() {
+		const isActive = ref(true);
+		const hasError = ref(false);
+		// const classObject = reactive({
+		// 	active: true,
+		// 	'text-danger': false,
+		// });
+
+		const classObject = computed(() => {
+			return {
+				active: true && true,
+				'text-danger': true && true,
+			};
+		});
+
+		const activeClass = ref('active');
+		const errorClass = ref('error');
+
+		const toggle = () => {
+			isActive.value = !isActive.value;
+		};
+		return {
+			isActive,
+			hasError,
+			classObject,
+			toggle,
+			activeClass,
+			errorClass,
+		};
+	},
+};
+</script>
+
+<style scoped>
+.active {
+	font-weight: 900;
+}
+
+.text-danger {
+	color: red;
+}
+</style>
---
+++ src/lectures/9/App_style_binding.vue
@@ -0,0 +1,35 @@
+<template>
+	<div>
+		<div :style="styleObject">
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
+			temporibus delectus eveniet quas modi inventore ab velit quam! Quis minus
+			vitae dicta earum quisquam ad sequi ipsa doloribus ipsam iusto.
+		</div>
+		<button v-on:click="fontSize--">-</button>
+		<button v-on:click="fontSize++">+</button>
+	</div>
+</template>
+
+<script>
+import { computed } from '@vue/reactivity';
+import { reactive, ref } from 'vue';
+
+export default {
+	setup() {
+		// const styleObject = reactive({
+		// 	color: 'red',
+		// 	fontSize: '13px',
+		// });
+		const fontSize = ref(13);
+		const styleObject = computed(() => {
+			return {
+				color: 'red',
+				fontSize: fontSize.value + 'px',
+			};
+		});
+		return { styleObject, fontSize };
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>

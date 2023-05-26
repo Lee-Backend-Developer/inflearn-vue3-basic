e8ab2855e7e9df3edbc333e750771061a300073c
Lee-Backend-Developer <eotmd1016@gmail.com>
2023-05-26 15:06:58 +0900

    디렉티브

---
+++ src/lectures/12/App_v-memo.vue
@@ -0,0 +1,32 @@
+<template>
+	<div>
+		<div v-memo="[subcribers, views, likes]">
+			<p>subcribers: {{ subcribers }}</p>
+			<p>views: {{ views }}</p>
+			<p>likes: {{ likes }}</p>
+		</div>
+		<button @click="subcribers++">Subs++</button>
+		<button @click="views++">views++</button>
+		<button @click="likes++">likes++</button>
+		<div>
+			<p>subcribers: {{ subcribers }}</p>
+			<p>views: {{ views }}</p>
+			<p>likes: {{ likes }}</p>
+		</div>
+	</div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+	setup() {
+		const subcribers = ref(4000);
+		const views = ref(400);
+		const likes = ref(20);
+		return { subcribers, views, likes };
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>

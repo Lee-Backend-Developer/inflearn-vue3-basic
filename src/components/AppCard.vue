79c508d5bda53a75cad277edb0d1c8ba69734a5a
Lee-Backend-Developer <eotmd1016@gmail.com>
2023-05-31 16:50:58 +0900

    Props

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -1,26 +1,61 @@
 <template>
 	<div class="card">
 		<div class="card-body">
-			<h5 class="card-title red">Card title</h5>
+			<!-- type : news , notice -->
+			<span class="badge bg-secondary">
+				{{ typeName }}
+			</span>
+			<h5 class="card-title red mt-2">{{ title }}</h5>
 			<p class="card-text">
-				Some quick example text to build on the card title and make up the bulk
-				of the card's content.
+				{{ contents }}
 			</p>
-			<a href="#" class="btn btn-primary">Go somewhere</a>
+			<a href="#" class="btn" :class="isLikeClass">좋아요</a>
 		</div>
 	</div>
 </template>
 
 <script>
+import { computed } from '@vue/reactivity';
+
 export default {
-	setup() {
-		return {};
+	props: {
+		type: {
+			type: String,
+			default: 'news',
+			validator: value => {
+				return ['news', 'notice'].includes(value);
+			},
+		},
+		title: {
+			type: String,
+			required: true,
+		},
+		contents: {
+			type: String,
+			required: true,
+		},
+		isLike: {
+			type: Boolean,
+			defalut: false,
+		},
+		obj: {
+			type: Object,
+			default: () => {},
+		},
+	},
+	setup(props) {
+		console.log('props : ', props.title);
+
+		const isLikeClass = computed(() =>
+			props.isLike ? 'btn-danger' : 'btn-outline-danger',
+		);
+
+		const typeName = computed(() =>
+			props.type === 'news' ? '뉴스' : '공지사항',
+		);
+		return { isLikeClass, typeName };
 	},
 };
 </script>
 
-<style scoped>
-.red {
-	color: red !important;
-}
-</style>
+<style scoped></style>

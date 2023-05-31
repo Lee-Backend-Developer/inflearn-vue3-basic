e3d64d8d5ce54fb535002f1b9eed1448862f5ee1
Lee-Backend-Developer <eotmd1016@gmail.com>
2023-05-31 14:12:54 +0900

    Single File Component

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { createApp } from 'vue';
 import App from './lectures/App.vue';
 
-createApp(App).mount('#app');
+const app = createApp(App);
+
+// app.component('AppCard', AppCard);
+
+app.mount('#app');
+import 'bootstrap/dist/js/bootstrap.js';
